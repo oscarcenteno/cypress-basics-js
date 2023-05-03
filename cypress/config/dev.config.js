01198abb53804ad81f085818caf40d11f4da9533
Oscar Centeno <oscar.centeno@outlook.com>
2023-05-03 16:30:02 +0200

    fixing typo

---
+++ cypress/config/dev.config.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  e2e: {
+    baseUrl: 'https://ej2.syncfusion.com/showcase/angular/appointmentplanner/#',
+    env: {
+      MY_ENVIRONMENT_VARIABLE: 'test'
+    },
+    setupNodeEvents (on, config) {
+    }
+  }
+}
